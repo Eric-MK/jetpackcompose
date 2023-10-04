ffc655d8dedba5d4f7d7cab184e9f61e5c55a373
unknown <mutungaeric839@gmail.com>
2023-10-04 19:09:16 +0300

    material design theme

--- app/src/main/java/com/example/myapplication_jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/myapplication_jetpackcompose/MainActivity.kt
@@ -12,21 +12,30 @@ import androidx.compose.ui.res.painterResource
 import com.example.myapplication_jetpackcompose.R
 
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
+import com.example.myapplication_jetpackcompose.ui.theme.MyApplication_jetpackcomposeTheme
+
 // ...
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(Message("Android", "Jetpack Compose"))
+            MyApplication_jetpackcomposeTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    MessageCard(Message("Android", "Jetpack Compose"))
+                }
+            }
         }
     }
 }
@@ -61,8 +70,12 @@ fun MessageCard(msg: Message) {
 @Preview
 @Composable
 fun PreviewMessageCard() {
-    MessageCard(
-        msg = Message("Lexi", "Hey, take a look at Jetpack Compose, it's great!")
-    )
+    MyApplication_jetpackcomposeTheme {
+        Surface {
+            MessageCard(
+                msg = Message("Lexi", "Take a look at Jetpack Compose, it's great!")
+            )
+        }
+    }
 }
 
