3ef305038d6f4e52ac122d81cee6e7c6418cc99f
unknown <mutungaeric839@gmail.com>
2023-10-04 14:25:35 +0300

    adding some comments for understanding

--- app/src/main/java/com/example/myapplication_jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/myapplication_jetpackcompose/MainActivity.kt
@@ -2,13 +2,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Text
-// ...
+// ... Other imports for Jetpack Compose
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Set the content of this activity to the MessageCard Composable
         setContent {
             MessageCard("Android")
         }
@@ -17,13 +18,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MessageCard(name: String) {
-    Text(text = "Hello $name!")
+    // Define a Composable function called MessageCard that takes a 'name' parameter
+    Text(text = "Hello $name!") // Display a Text Composable with a greeting message
 }
 
 @Preview
 @Composable
 fun PreviewMessageCard() {
-    MessageCard("Android")
+    // Define a Composable function called PreviewMessageCard
+    MessageCard("Android") // Display the MessageCard Composable with a specific 'name'
 }
-
-  
