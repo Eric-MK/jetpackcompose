c79fb32daf3e887b8674b5dc7a019b4b96fef8e4
unknown <mutungaeric839@gmail.com>
2023-10-04 19:29:36 +0300

    material design theme color scheme

--- app/src/main/java/com/example/myapplication_jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/myapplication_jetpackcompose/MainActivity.kt
@@ -23,7 +23,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import com.example.myapplication_jetpackcompose.ui.theme.MyApplication_jetpackcomposeTheme
-
+import androidx.compose.foundation.border
+import androidx.compose.material3.MaterialTheme
 // ...
 
 
@@ -44,29 +45,30 @@ data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(msg: Message) {
-    // Add padding around our message
     Row(modifier = Modifier.padding(all = 8.dp)) {
         Image(
             painter = painterResource(R.drawable.profile_picture),
-            contentDescription = "Contact profile picture",
+            contentDescription = null,
             modifier = Modifier
-                // Set image size to 40 dp
                 .size(40.dp)
-                // Clip image to be shaped as a circle
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
         )
 
-        // Add a horizontal space between the image and the column
         Spacer(modifier = Modifier.width(8.dp))
 
         Column {
-            Text(text = msg.author)
-            // Add a vertical space between the author and message texts
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colorScheme.secondary
+            )
+
             Spacer(modifier = Modifier.height(4.dp))
             Text(text = msg.body)
         }
     }
 }
+
 @Preview
 @Composable
 fun PreviewMessageCard() {
